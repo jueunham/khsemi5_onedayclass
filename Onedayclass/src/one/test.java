95178b5bb43c4c12aa8c965a5dbf3c7fcf00dcfb
user1 <sara710@naver.com>
2019-05-27 14:32:43 +0900

    test11111

--- Onedayclass/src/one/test.java
+++ Onedayclass/src/one/test.java
@@ -1,9 +1,8 @@
 package one;
 
 public class test {
- // test �뙆�씪 �엯�땲�떎.
+ // test 파일 입니다.
 	//kjhnkjhkjhku
-	//testingtestingtesting
-	//testingtestingtestingggggggg
-	//수정수정수정
+	
+	//askdjflakjgoewjogfjaowejifoajsdo
 }
