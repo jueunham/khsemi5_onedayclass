78fa08ad19fbe2824531342929645444d9728175
YiYu <ssosso12347@gmail.com>
2019-06-08 16:06:49 +0900

    user mypage 진행 중
    user mypage writtenlist 진행 중

--- Onedayclass/src/one/service/face/MypageService.java
+++ Onedayclass/src/one/service/face/MypageService.java
@@ -1,5 +1,23 @@
 package one.service.face;
 
-public interface MypageService {
+import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import one.dto.Member;
+
+
+public interface MypageService {
+	
+	public List<Map<String, Object>> getList();
+	
+	public Member getUserNum(HttpServletRequest req);
+	
+	public Member view(Member member);
+	
+	public void updateInfo(HttpServletRequest req);
+	
+	public List<Map<String, Object>> getWishList();
+	
 }
